ea7cdd99a30d0c0e1ab1a41efb15ce7c404dd5c3
amoretto <amoretto@outlook.com.br>
2021-03-27 15:31:41 -0300

    API Starting And Database, Methods Get and Get(id) Ok in Async

---
+++ ProAgilAPI/ProAgilAPI/Data/ProAgilApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAgilAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAgilAPI.Data
+{
+    public class ProAgilApiContext : DbContext
+    {
+        public ProAgilApiContext(DbContextOptions<ProAgilApiContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ ProAgilAPI/ProAgilAPI/Model/Event.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ProAgilAPI.Model
+{
+    public class Event
+    {
+        [Key]
+        public int IdEvent { get; set; }
+        public string Local { get; set; }
+        public string EventDate { get; set; }
+        public string Subject { get; set; }
+        public int GuestsQty { get; set; }
+        public string Lot { get; set; }
+
+        public Event() { }
+    }
+}
